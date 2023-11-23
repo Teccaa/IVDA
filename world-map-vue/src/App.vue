480da9b5e8b67bf0b939076ab366a4976522d208
Teccaa <remo.bruhin@uzh.ch>
2023-11-23 11:31:00 +0100

    Hovering Info

--- world-map-vue/src/App.vue
+++ world-map-vue/src/App.vue
@@ -8,18 +8,17 @@
       <i class="fas fa-question-circle"></i>
     </div>
     <WorldMap :icons="icons" />
-    <IconContainer :icons="icons" />
+    <IconContainer :icons="iconsWithDescriptions" />
     <StatisticsTable :documents="documents" />
     <ScoreGraph />
   </div>
 </template>
 
-
 <script>
 import WorldMap from "@/components/WorldMap.vue";
 import IconContainer from "@/components/IconContainer.vue";
 import StatisticsTable from "@/components/StatisticsTable.vue";
-import ScoreGraph from "@/components/ScoreGraph.vue"; // 瀵煎叆Graph缁勪欢
+import ScoreGraph from "@/components/ScoreGraph.vue";
 
 export default {
   components: {
@@ -34,10 +33,28 @@ export default {
         id: i + 1,
         path: require(`@/assets/icons/icon${i + 1}.png`),
       })),
+      iconsWithDescriptions: [
+        { id: 1, path: require(`@/assets/icons/icon1.png`), description: 'No poverty' },
+        { id: 2, path: require(`@/assets/icons/icon2.png`), description: 'Zero hunger' },
+        { id: 3, path: require(`@/assets/icons/icon3.png`), description: 'Good health and wellbeing' },
+        { id: 4, path: require(`@/assets/icons/icon4.png`), description: 'Quality education' },
+        { id: 5, path: require(`@/assets/icons/icon5.png`), description: 'Gender equality' },
+        { id: 6, path: require(`@/assets/icons/icon6.png`), description: 'Clean water and sanitation' },
+        { id: 7, path: require(`@/assets/icons/icon7.png`), description: 'Affordable and clean energy' },
+        { id: 8, path: require(`@/assets/icons/icon8.png`), description: 'Decent work and economic growth' },
+        { id: 9, path: require(`@/assets/icons/icon9.png`), description: 'Industry, innovation and infrastructure' },
+        { id: 10, path: require(`@/assets/icons/icon10.png`), description: 'Reduced inequalities' },
+        { id: 11, path: require(`@/assets/icons/icon11.png`), description: 'Sustainable cities and communities' },
+        { id: 12, path: require(`@/assets/icons/icon12.png`), description: 'Responsible consumption and production' },
+        { id: 13, path: require(`@/assets/icons/icon13.png`), description: 'Climate action' },
+        { id: 14, path: require(`@/assets/icons/icon14.png`), description: 'Life below water' },
+        { id: 15, path: require(`@/assets/icons/icon15.png`), description: 'Life on land' },
+        { id: 16, path: require(`@/assets/icons/icon16.png`), description: 'Peace, justice and strong institutions' },
+        { id: 17, path: require(`@/assets/icons/icon17.png`), description: 'Partnerships for the goals' },
+      ],
       documents: [
         { id: 1, title: "Document 1", author: "Author 1" },
         { id: 2, title: "Document 2", author: "Author 2" },
-
       ],
     };
   },
@@ -76,7 +93,7 @@ export default {
 .icon-container {
   position: fixed;
   bottom: 0;
-  width: 100%;
+  width: 1025px;
 }
 
 .score-graph {
--- world-map-vue/src/components/IconContainer.vue
+++ world-map-vue/src/components/IconContainer.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="icon-container">
-    <div v-for="icon in icons" :key="icon.id" class="icon-item">
-      <img :src="icon.path" alt="icon" />
+    <div
+      v-for="icon in icons"
+      :key="icon.id"
+      class="icon-item"
+      @mouseover="showIconInfo(icon)"
+      @mouseout="hideIconInfo"
+    >
+      <img :src="generateIconPath(icon.id)" alt="icon" class="icon-image" />
+      <div v-if="icon === hoveredIcon" class="icon-info">
+        <div class="icon-description">{{ icon.description }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,26 +20,59 @@ export default {
   props: {
     icons: Array,
   },
+  data() {
+    return {
+      hoveredIcon: null,
+    };
+  },
+  methods: {
+    showIconInfo(icon) {
+      this.hoveredIcon = icon;
+    },
+    hideIconInfo() {
+      this.hoveredIcon = null;
+    },
+    generateIconPath(id) {
+      return require(`@/assets/icons/icon${id}.png`);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .icon-container {
   display: flex;
-  overflow-x: auto;
+  flex-wrap: wrap;
+  justify-content: center;
   padding: 10px;
 }
 
 .icon-item {
-  margin-right: 0px;
+  position: relative;
+  margin: 6px;
 }
 
-.icon-item:last-child {
-  margin-right: 0;
+.icon-item .icon-image {
+  width: 80px;
+  height: 60px;
 }
 
-.icon-item img {
-  width: 60px;
-  height: 40px;
+.icon-info {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 8px;
+  border-radius: 4px;
+  display: none;
+}
+
+.icon-info .icon-description {
+  font-weight: bold;
+}
+
+.icon-item:hover .icon-info {
+  display: block;
 }
 </style>
