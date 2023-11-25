b7be4ee1702e1035fe66494dd619851b71121b11
Simon Hafner <simon.hafner3@uzh.ch>
2023-11-25 17:38:27 +0100

    Merged projects
    World map and bar plot are now integrated in one web app.

---
+++ services/frontend/world-map-vue/src/App.vue
@@ -0,0 +1,84 @@
+<!-- App.vue -->
+
+<template>
+  <div id="app">
+    <div class="search-box">
+      <i class="fas fa-search"></i>
+      <input type="text" placeholder="Search for documents with keywords" />
+      <i class="fas fa-question-circle"></i>
+    </div>
+    <IconContainer :icons="icons" />
+    <WorldMap :icons="icons" />
+    <StatisticsTable :documents="documents" />
+    <ScoreGraph :data="selectedSDG"/>
+  </div>
+</template>
+
+
+<script>
+import WorldMap from "@/components/WorldMap.vue";
+import StatisticsTable from "@/components/StatisticsTable.vue";
+import IconContainer from "@/components/IconContainer.vue";
+import ScoreGraph from "@/components/ScoreGraph.vue";
+
+export default {
+  components: {
+    WorldMap,
+    IconContainer,
+    StatisticsTable,
+    ScoreGraph
+  },
+  data() {
+    return {
+      icons: Array.from({ length: 17 }, (_, i) => ({
+        id: i + 1,
+        path: require(`@/assets/icons/icon${i + 1}.png`),
+      })),
+      documents: [
+        { id: 1, title: "Document 1", author: "Author 1" },
+        { id: 2, title: "Document 2", author: "Author 2" },
+
+      ],
+      selectedSDG: 'sdg1_avg'
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  position: relative;
+}
+
+.search-box {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+}
+
+.search-box i {
+  margin: 0 10px;
+}
+
+.search-box input {
+  flex: 1;
+  padding: 8px;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 5px;
+}
+
+.icon-container {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+
+</style>
---
+++ services/frontend/world-map-vue/src/components/ScoreGraph.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h2></h2>
+    <div ref="plotlyChart"></div>
+    
+    <div id='myDiv' style="position: fixed; top: 0; right: 0; height: 60vh;"></div>
+
+
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+import Plotly from 'plotly.js/dist/plotly';
+
+function getTop5Countries(data, SDG) {
+  // Sort the data by the selected SDG
+  const sortedData = Array.isArray(data) ? data.sort((a, b) => b[SDG] - a[SDG]) : [];
+
+  // Slice the top 5 countries
+  const top5Countries = sortedData.slice(0, 120);
+  const top5CountriesWithSelectedScore = top5Countries.map(countryData => {
+    return {
+      country: countryData["Country"],
+      SDG: countryData[SDG]
+    };
+  });
+  return top5CountriesWithSelectedScore;
+}
+
+export default {
+  data: () => ({
+    data: [],
+  }),
+  props: {
+    chartTitle: {
+      type: String,
+      default: 'Bar Plot',
+    },
+  },
+  mounted() {
+    this.fetchData();
+    this.drawBarPlot();
+  },
+  watch: {
+    data: 'drawBarPlot',
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get("/summary_table.json");
+        console.log(response.data);
+        this.data = response.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+
+        // Log the complete error object for more details
+        console.log(error);
+
+        // Alternatively, log specific properties of interest
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("Response data:", error.response.data);
+          console.log("Response status:", error.response.status);
+          console.log("Response headers:", error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log("Request made but no response received:", error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error setting up the request:", error.message);
+        }
+      }
+    },
+    drawBarPlot() {
+      const selectedSDG = 'sdg14_avg';
+        
+      const top5CountriesData = getTop5Countries(this.data, selectedSDG);
+      console.log(this.data);
+      let data = [
+        {
+          //x: countries,
+          //y: scores,
+          x: top5CountriesData.map(countryData => countryData.country),
+          y: top5CountriesData.map(countryData => countryData.SDG),
+          type: 'bar'
+        }
+      ];
+      const layout = {
+        title: "Top 5 Countries for SDG(insert the selected SDG Variable here)",
+        xaxis: {
+          title: "Countries ranked (descending)"
+        },
+        yaxis: {
+          title: "Average SDG Score"
+        }
+      };
+      const config = {
+        responsive: true,
+      };
+      Plotly.newPlot("myDiv", data, layout, config);
+    }
+
+  }
+};
+
+
+</script>
