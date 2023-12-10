48dd3ac7d66886961c940c8546e7489b45785d4c
Teccaa <remo.bruhin@uzh.ch>
2023-12-10 18:35:19 +0100

    Banner Description

--- services/frontend/world-map-vue/src/App.vue
+++ services/frontend/world-map-vue/src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <div class="banner">
+    <div class="banner" @click="toggleBanner">
       <img src="@/assets/NEW_LOGO.png" alt="SDG Goals Image" />
-
+      <div v-if="isBannerExpanded" class="banner-description">
+        <strong>Description:</strong>
+        <br>
+        This dashboard features a a world map with 258 countries.
+        <br>
+        It illustrates the variance in how UZH research covers Sustainable Development Goals (SDGs) with respect to 165 countries which were referenced in the abstracts UZH research publications.
+      </div>
     </div>
+    <div v-if="!isBannerExpanded" class="expand-icon" @click="toggleBanner">
+      +
+    </div>
+
     <div class="dropdown">
       <label for="nrOfCountries">Number of Countries: </label>
       <select id="nrOfCountries" v-model="NrOfCountries.selectedValue">
@@ -12,6 +22,7 @@
         </option>
       </select>
     </div>
+
     <div class="SGDdropdown">
       <label for="SDG">Number of SDG: </label>
       <select id="SDG" v-model="NrOfSDG.selectedValue">
@@ -27,8 +38,7 @@
       @SDG-icon-name="handleSDGIconName"
     />
 
-    <WorldMap :selectedSDG="selectedSDG" @area-selected="handleAreaSelected"
-    />
+    <WorldMap :selectedSDG="selectedSDG" @area-selected="handleAreaSelected" />
 
     <ScoreGraph
       :selectedSDG="selectedSDG"
@@ -40,14 +50,11 @@
       :selected-value="Number(NrOfSDG.selectedValue)"
       @area-selected="handleAreaSelected"
     />
-
-    />
   </div>
 </template>
 
 <script>
 import WorldMap from "@/components/WorldMap.vue";
-// import StatisticsTable from "@/components/StatisticsTable.vue";
 import IconContainer from "@/components/IconContainer.vue";
 import ScoreGraph from "@/components/ScoreGraph.vue";
 import SDGGraph from "@/components/SDGGraph.vue";
@@ -57,11 +64,11 @@ export default {
     SDGGraph,
     WorldMap,
     IconContainer,
-    // StatisticsTable,
     ScoreGraph,
   },
   data() {
     return {
+      isBannerExpanded: false,
       NrOfCountries: {
         values: [5, 10, 20, 50, 100],
         selectedValue: 5,
@@ -74,10 +81,6 @@ export default {
         id: i + 1,
         path: require(`@/assets/icons/icon${i + 1}.png`),
       })),
-      documents: [
-        { id: 1, title: "Document 1", author: "Author 1" },
-        { id: 2, title: "Document 2", author: "Author 2" },
-      ],
       selectedSDG: {
         id: 1,
         name: "No Poverty",
@@ -89,13 +92,14 @@ export default {
     };
   },
   methods: {
+    toggleBanner() {
+      this.isBannerExpanded = !this.isBannerExpanded;
+    },
     handleSDGIconId(sdg_id) {
       this.selectedSDG.id = sdg_id;
-      // console.log(`Updated selected SDG-ID: ${this.selectedSDG.id}.`);
     },
     handleSDGIconName(sdg_name) {
       this.selectedSDG.name = sdg_name;
-      // console.log(`Updated SDG-name: ${this.selectedSDG.name}.`);
     },
     handleAreaSelected(selectedArea) {
       this.selectedArea = selectedArea;
@@ -111,35 +115,67 @@ export default {
 }
 
 .banner {
-  /* Banner Title for the UN SDG Goals */
-
-  position: absolute;
-  top: 0;
-  left: 0;
+  position: relative;
   width: 100%;
   height: 50px;
   background-color: #ccc;
-  z-index: 1000;
   display: flex;
   align-items: center;
   justify-content: center;
   border: 1.5px solid #666;
   box-sizing: border-box;
+  cursor: pointer;
+  z-index: 1999;
 }
 
 .banner img {
   max-width: 21%;
   z-index: 2000;
 }
-/* Number of Countries */
+
+.banner-description {
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  background-color: #ccc; 
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  border: 1.5px solid #666;
+  padding: 10px;
+  box-sizing: border-box;
+  z-index: 100;
+  font-size: 16px; 
+}
+
+.expand-icon {
+  position: absolute;
+  top: 50%;
+  left: 62%; 
+  transform: translate(-50%, -50%);
+  font-size: 20px;
+  color: #fff; 
+  background-color: #ccc; 
+  width: 30px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1.5px solid #666;
+  border-radius: 50%;
+  z-index: 2000;
+  cursor: pointer;
+}
+
+
 .dropdown {
   position: fixed;
-
   top: 100px;
   right: 50px;
   z-index: 1000;
   font-family: sans-serif;
 }
+
 .dropdown label {
   margin-right: 5px;
 }
@@ -147,7 +183,7 @@ export default {
 .dropdown select {
   padding: 5px;
 }
-/* Number of SDG */
+
 .SGDdropdown {
   position: fixed;
   top: 700px;
@@ -155,9 +191,11 @@ export default {
   z-index: 1000;
   font-family: sans-serif;
 }
+
 .SGDdropdown label {
   margin-right: 5px;
 }
+
 .SGDdropdown select {
   padding: 5px;
 }
@@ -168,4 +206,5 @@ export default {
   width: 100%;
 }
 
+/* ... any additional styles you may have ... */
 </style>
